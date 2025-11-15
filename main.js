c34a7c03b35cd369436ccdea90f830e3c0b4d123
Maria <63619962+MarijaAvramovic@users.noreply.github.com>
2025-11-15 09:36:52 -0500

    reset board values

--- main.js
+++ main.js
@@ -1,93 +1,118 @@
 
-// factories
-function gameboardObj () {
+// // factories
+// function gameboardObj () {
 
  
-    const gameboard = ['', '', '', '', '', '', '', '', ''];
+//     const gameboard = ['', '', '', '', '', '', '', '', ''];
      
-    return{gameboard};
+//     return{gameboard};
 
      
-}
-function player (sign) {
-    return {}; 
-};
-// game flow obj
-function game () {
-    return {};
-} ;
+// }
+// function player (sign) {
+//     return {}; 
+// };
+// // game flow obj
+// function game () {
+//     return {};
+// } ;
 
-// iife
-const displayController = (function () {
+// // iife
+// const displayController = (function () {
 
-}
+// }
 
-)();
+// )();
 
  
-console.log(gameboardObj());
+// console.log(gameboardObj());
 
 
-console.log(gameboardObj());
+// console.log(gameboardObj());
 
 
-let igrac = "x";
-function changePlayer(){
-    if(igrac == "x"){
-        igrac = "o";
-    }
-    else if(igrac == "o"){
-        igrac = "x";
-    }
-}
-let board = [
-    '', '', '',
-     '', '', '', 
-     '', '', ''];
-// changePlayer();
-// console.log(igrac);
+// let igrac = "x";
+// function changePlayer(){
+//     if(igrac == "x"){
+//         igrac = "o";
+//     }
+//     else if(igrac == "o"){
+//         igrac = "x";
+//     }
+// }
+// let board = [
+//     '', '', '',
+//      '', '', '', 
+//      '', '', ''];
+// // changePlayer();
+// // console.log(igrac);
 
-for (let i = 0; i < board.length; i++) {
-        board[i] = igrac; 
-        changePlayer();
-    }
+// for (let i = 0; i < board.length; i++) {
+//         board[i] = igrac; 
+//         changePlayer();
+//     }
 
-    console.log(board);
+//     console.log(board);
 
 
-let winningDraws = [
-    [board[0], board[3], board[6]],
-    [board[1], board[4], board[7]],
-    [board[2], board[5], board[8]],
-    [board[0], board[4], board[6]],
-    [board[0], board[1], board[2]],
-    [board[3], board[4], board[5]],
-    [board[6], board[7], board[8]]
-];
-console.log(winningDraws);
+// let winningDraws = [
+//     [board[0], board[3], board[6]],
+//     [board[1], board[4], board[7]],
+//     [board[2], board[5], board[8]],
+//     [board[0], board[4], board[6]],
+//     [board[0], board[1], board[2]],
+//     [board[3], board[4], board[5]],
+//     [board[6], board[7], board[8]]
+// ];
+// console.log(winningDraws);
 
-const winnerDraw = winningDraws.filter(function(element) {
-    //arr[0]; 3 elementa
-    const firstElement = element[0];
-    return element.every(element => element === firstElement);
+// const winnerDraw = winningDraws.filter(function(element) {
+//     //arr[0]; 3 elementa
+//     const firstElement = element[0];
+//     return element.every(element => element === firstElement);
+// });
+
+// console.log(winnerDraw);
+
+// //  const [winner] = winnerDraw;
+//  let winner = winnerDraw[0][0];
+
+//  console.log(winner);
+
+//  function reset() {
+//     winner = '';
+//     board = [
+//     '', '', '',
+//      '', '', '', 
+//      '', '', ''];
+//      igrac = "x";
+//  }
+
+// reset();
+
+//  console.log(board);
+
+ // html
+
+ let cells = document.querySelectorAll('.item');
+ let boardArray = [];
+
+cells.forEach(div => {
+  boardArray.push(div.textContent); 
 });
 
-console.log(winnerDraw);
+console.log(boardArray);
 
-//  const [winner] = winnerDraw;
- let winner = winnerDraw[0][0];
-
- console.log(winner);
-
- function reset() {
-    winner = '';
-    board = [
+function reset(){
+    boardArray = [
     '', '', '',
      '', '', '', 
      '', '', ''];
-     igrac = "x";
- }
+     cells.forEach(cell => {
+        cell.textContent = '';
+     });
+    // cells.textContent = "";
+}
 
 reset();
-
- console.log(board);
+console.log(boardArray);
