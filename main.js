71596b97b8bc4f4270a87988d0c7a33c343be16e
Maria <63619962+MarijaAvramovic@users.noreply.github.com>
2025-11-16 11:21:37 -0500

    winning combination

--- main.js
+++ main.js
@@ -93,12 +93,25 @@
 //  console.log(board);
 
  // html
+    const winnerMsg = document.querySelector(".winner");
 
+    boardArray = [
+    '', '', '',
+     '', '', '', 
+     '', '', ''];
 
+    
+
+    let player = 'X';
+    function changePlayer() {
+        player = (player == 'X') ? "O" : "X";
+    }
+    changePlayer();
+    console.log(player);
 
  let cells = document.querySelectorAll('.item');
  
- let boardArray = [];
+ 
 
  const btnReset = document.querySelector('.reset');
 console.log(btnReset);
@@ -106,12 +119,41 @@ console.log(btnReset);
 btnReset.addEventListener("click", function() {
     reset();
   });
-cells.forEach(div => {
-  boardArray.push(div.textContent); 
-});
 
-console.log(boardArray);
+ 
 
+    cells.forEach(cell => {
+      cell.addEventListener('click', event => {
+        if(event.target.textContent === ''){    
+
+            checkCells(boardArray);
+            event.target.textContent = player;
+            changePlayer();
+              const  cellId = event.target.dataset.index; 
+              boardArray[cellId] = event.target.textContent;
+
+            console.log(boardArray);
+
+        }
+         
+      });
+    });
+
+
+ let table = {
+    zero: document.querySelector('.zero').textContent,
+    first: document.querySelector('.one').textContent,
+    second: document.querySelector('.two').textContent,
+    third: document.querySelector('.three').textContent,
+    fourth: document.querySelector('.four').textContent,
+    fifth: document.querySelector('.five').textContent,
+    sixth: document.querySelector('.six').textContent,
+    seventh: document.querySelector('.seven').textContent,
+    eight: document.querySelector('.eight').textContent
+
+ }
+
+ console.log(table);
 function reset(){
     boardArray = [
     '', '', '',
@@ -124,4 +166,37 @@ function reset(){
 }
 
  
-console.log(boardArray);
+let winningDraws = [
+    [boardArray[0], boardArray[3], boardArray[6]],
+    [boardArray[1], boardArray[4], boardArray[7]],
+    [boardArray[2], boardArray[5], boardArray[8]],
+    [boardArray[0], boardArray[4], boardArray[6]],
+    [boardArray[0], boardArray[1], boardArray[2]],
+    [boardArray[3], boardArray[4], boardArray[5]],
+    [boardArray[6], boardArray[7], boardArray[8]],
+    [boardArray[0], boardArray[4], boardArray[8]],
+    [boardArray[2], boardArray[4], boardArray[6]]
+];
+ 
+
+function checkCells(){
+        
+        
+    
+    
+    } ;
+
+ 
+console.log(winningDraws);
+
+const winnerDraw = winningDraws.filter(function(element) {
+    //arr[0]; 3 elementa
+    const firstElement = element[0];
+    return element.every(element => element === firstElement);
+});
+
+console.log(winnerDraw);
+
+//  const [winner] = winnerDraw;
+ let winner = winnerDraw[0][0];
+ console.log(winner);
