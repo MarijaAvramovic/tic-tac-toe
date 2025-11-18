0e60eba27761458420d26d3224957e1212c1ef18
Maria <63619962+MarijaAvramovic@users.noreply.github.com>
2025-11-18 10:37:40 -0500

    find winner

--- main.js
+++ main.js
@@ -4,11 +4,54 @@ const btnStart = document.querySelector("#start");
 const o = gameboard();
 console.log(o.cells);
 console.log(o.moves);
+console.log(o);
 
+ const arr = o.moves;
  
- 
- 
+ console.log(arr);
 
+  console.log(checkWinner(o.moves));
+
+    
+//    let ge = [
+//     [arr[0], arr[3], arr[6]],
+//     [arr[1], arr[4], arr[7]],
+//     [arr[2], arr[5], arr[8]],
+//     [arr[0], arr[4], arr[6]],
+//     [arr[0], arr[1], arr[2]],
+//     [arr[3], arr[4], arr[5]],
+//     [arr[6], arr[7], arr[8]]
+// ];
+
+// console.log(ge);
+
+//  const winnerArr = ge.filter(function(element) {
+         
+//         const firstElement = element[0];
+
+//   return element.every(e => e === firstElement);
+//         });
+        console.log('s');
+
+function checkWinner(arr) {
+    let combos = [
+    [arr[0], arr[3], arr[6]],
+    [arr[1], arr[4], arr[7]],
+    [arr[2], arr[5], arr[8]],
+    [arr[0], arr[4], arr[6]],
+    [arr[0], arr[1], arr[2]],
+    [arr[3], arr[4], arr[5]],
+    [arr[6], arr[7], arr[8]]
+];
+ 
+   const winnerArr = combos.filter(function(element) {
+         const firstElement = element[0];
+
+        return element.every(e => e === firstElement);
+   });
+        
+        return {winnerArr}
+}
 
 function gameboard() {
     const cells = document.querySelectorAll('.item');
@@ -18,6 +61,10 @@ function gameboard() {
         const move = element.textContent;
         moves.push(move);
     });
+
+     
+    
     return {cells, moves }
 }
 
+ 
