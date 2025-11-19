e97ddce7c05c7eee0536d291a0f431f387a5c3e0
Maria <63619962+MarijaAvramovic@users.noreply.github.com>
2025-11-18 23:55:04 -0500

    update readme

--- main.js
+++ main.js
@@ -173,14 +173,16 @@ function announceNames(nameX, nameO) {
 
 function checkWinner(arr) {
     let combos = [
+        
     [arr[0], arr[3], arr[6]],
-    [arr[0], arr[4], arr[6]],
+    [arr[0], arr[4], arr[8]],
     [arr[0], arr[1], arr[2]],
     [arr[1], arr[4], arr[7]],
-    [arr[2], arr[5], arr[8]],
     [arr[2], arr[4], arr[6]],
     [arr[3], arr[4], arr[5]],
-    [arr[6], arr[7], arr[8]]
+    [arr[6], arr[7], arr[8]],
+    [arr[2], arr[5], arr[8]]
+     
 ];
     
     
@@ -193,6 +195,7 @@ function checkWinner(arr) {
          
          
    });
+ 
 
    
    const winnerSign = winnerArr[0][0];
